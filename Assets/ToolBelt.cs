6586332f3ee1badb556dad82f37ec2dca01eae4a
Dobrin-Cotea-Antonio <534902@student.saxion.nl>
2024-05-13 15:35:28 +0200

    Tool Belt Functional

--- Assets/ToolBelt.cs
+++ Assets/ToolBelt.cs
@@ -6,9 +6,6 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class ToolBelt : MonoBehaviour {
     [SerializeField] CharacterController characterController;
     [SerializeField] [Tooltip("Percentage of the player height this will be placed at")] float heightPercentage;
-    //[SerializeField] Vector3 _attachPosition;
-
-    //public Vector3 attachPosition { get { return _attachPosition; } }
     Vector3 initialOffset;
 
     #region Unity Events
@@ -19,9 +16,6 @@ public class ToolBelt : MonoBehaviour {
 
     private void Update() {
         transform.localPosition = new Vector3(initialOffset.x + characterController.center.x, characterController.height * (heightPercentage / 100f), initialOffset.z + characterController.center.z);
-
-        //if (attachedTransform != null)
-        //   attachedTransform.localPosition = transform.localPosition;
     }
 
     private void OnTriggerEnter(Collider pOther) {
@@ -33,7 +27,6 @@ public class ToolBelt : MonoBehaviour {
         if (attachable.AttachedToolbelt() != null)
             return;
 
-        //Debug.Log("entered");
         attachable.Attach(this);
     }
 
@@ -45,8 +38,6 @@ public class ToolBelt : MonoBehaviour {
 
         if (attachable.AttachedToolbelt() != this)
             return;
-
-        //Debug.Log("exited");
 
         attachable.Detach(this);
     }
