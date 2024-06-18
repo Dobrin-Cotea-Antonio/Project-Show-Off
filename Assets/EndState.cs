415e4ed93242b79f66ad0ff37e21ebb58c8a62e6
Dobrin-Cotea-Antonio <534902@student.saxion.nl>
2024-06-18 14:17:25 +0200

    Changes

--- Assets/EndState.cs
+++ Assets/EndState.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.SceneManagement;
 
 public class EndState : GameState {
 
     [Header("End Data")]
     [SerializeField] XRSimpleInteractable gameEndInteractable;
+    [SerializeField] string endScene;
 
     #region State Handling
     public override void Handle() {
@@ -14,21 +16,21 @@ public class EndState : GameState {
     }
 
     public override void OnStateEnter() {
-
+        gameEndInteractable.selectEntered.AddListener(GoToEndScreen);
         // play voicelines
         SoundManager.PlaySoundRepeating(SoundManager.Sound.VoiceLine_PLAYER_WON, 10);
     }
 
     public override void OnStateExit() {
-
+        gameEndInteractable.selectEntered.RemoveListener(GoToEndScreen);
         // stop voicelines
         SoundManager.StopSound(SoundManager.Sound.VoiceLine_PLAYER_WON);
     }
     #endregion
 
     #region Helper Methods
-    private void GoToEndScreen() { 
-    
+    private void GoToEndScreen(SelectEnterEventArgs pArgs) {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(endScene);
     }
     #endregion
 }
