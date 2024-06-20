cf4aab3e6b62207b849ef42358acfd183125e745
Patricio <patriciogutierrez236@gmail.com>
2024-06-20 17:11:20 +0200

    Added a bunch

--- Assets/AttackMastState.cs
+++ Assets/AttackMastState.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class AttackMastState : EnemyState
 {
-    [Header("Attack Data")] [SerializeField]
-    float attackDelay;
-
+    [Header("Attack Data")]
     [SerializeField] float attackDamage;
 
     [Header("Animation")] [SerializeField] Animator animator;
     [SerializeField] GameObject weaponGameObject;
 
     MastScript mast;
-    float lastAttackTime = -100000000;
 
     #region Unity Events
 
@@ -28,10 +25,7 @@ public class AttackMastState : EnemyState
 
     public override void Handle()
     {
-        if (Time.time - lastAttackTime <= attackDelay)
-            return;
-
-        mast.TakeDamage(attackDamage);
+        LookAtMast();
 
         // play sawing mast sound
         SoundManager.PlaySound(SoundManager.Sound.SawingMast, transform);
@@ -48,17 +42,49 @@ public class AttackMastState : EnemyState
             SoundManager.PlaySound(SoundManager.Sound.VoiceLine_ENEMY_CUTTING_MAST, transform);
         }
 
-        lastAttackTime = Time.time;
+        //lastAttackTime = Time.time;
+    }
+
+    public void DamageMast()
+    {
+        mast.TakeDamage(attackDamage);
     }
 
     public override void OnStateEnter()
     {
         weaponGameObject.SetActive(false);
+
+        if (animator != null)
+        {
+            animator.SetBool("IsSawing", true);
+            Debug.Log("Set Sawing to true");
+        }
+        else
+        {
+            Debug.LogError("Animator is not assigned");
+        }
+
+        LookAtMast();
     }
 
     public override void OnStateExit()
     {
         weaponGameObject.SetActive(true);
+        animator.SetBool("IsSawing", false);
+    }
+
+    #endregion
+
+    #region Helper Methods
+
+    private void LookAtMast()
+    {
+        if (mast != null)
+        {
+            Vector3 direction = (mast.transform.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        }
     }
 
     #endregion
